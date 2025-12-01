cec7620ae9d9518779a801fabc6a09b3abc7768b
robert <robert.hak@tul.cz>
2025-12-01 20:08:52 +0100

    day2 innit

---
+++ day2/pt1.py
@@ -0,0 +1,11 @@
+def day2_pt1():
+    file = 'input'
+
+    with open(file, "r", encoding="utf-8") as f:
+        for raw_line in f:
+            line = raw_line.strip()
+            if not line:
+                continue
+
+if __name__ == '__main__':
+    day2_pt1()
